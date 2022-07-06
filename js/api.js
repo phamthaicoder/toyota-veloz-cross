3489c8c704221975446e40607bf1a13fad04a788
nambui <nambp@amaisoft.com>
2022-07-06 22:20:36 +0700

    check api

--- js/api.js
+++ js/api.js
@@ -1,9 +1,10 @@
 // // const tokenAccess = 
 
-// fetch('https://ssa-api.toyotavn.com.vn/api/services/app/SalesCustomerWeb/GetListDealers', {
-//     method: "GET",
+// fetch('https://ssa-api.toyotavn.com.vn/api/TokenAuth/Authenticate', {
+//     method: "POST",
 //     headers: new Headers({
-//         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjQzNzcwIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6IkFQSV9MRUFEX0hBS1VIT0tPIiwiQXNwTmV0LklkZW50aXR5LlNlY3VyaXR5U3RhbXAiOiJGV0hQWjRMMzJEVE9DM1RYM1JBUUVDRjVDQkZHU0JZNCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6WyJVc2VyIiwiMWE1NWFhYmNiM2MxNGNlNGIxOGI5YTBlNmQwOWRhNDAiXSwiaHR0cDovL3d3dy5hc3BuZXRib2lsZXJwbGF0ZS5jb20vaWRlbnRpdHkvY2xhaW1zL3RlbmFudElkIjoiMTAwNSIsInN1YiI6IjQzNzcwIiwianRpIjoiNGE0NzllNTctMDhhYy00YTBjLWE1MzgtYmFiNWY3M2U0MjM2IiwiaWF0IjoxNjU2NTgzNTA4LCJ0b2tlbl92YWxpZGl0eV9rZXkiOiJlYmI3N2IwNi1mMTBjLTQ1ZjUtOTg5Yi1kNDYxZDIwZjYyYzgiLCJ1c2VyX2lkZW50aWZpZXIiOiI0Mzc3MEAxMDA1IiwidG9rZW5fdHlwZSI6IjAiLCJuYmYiOjE2NTY1ODM1MDgsImV4cCI6MTY1NjYyMTMxMywiaXNzIjoidG1zcyIsImF1ZCI6InRtc3MifQ.7IoQ5ywXw4cswhy4s4S729IMdd-a5lpcZ36K6062gfk",
+//         'Abp.TenantId': '1005',
+//         'Content-Type': 'application/json'
 //     }),
 //     raw: {
 //         "userNameOrEmailAddress": "API_LEAD_HAKUHOKO",
@@ -11,6 +12,164 @@
 //     },
 //     mode: "no-cors",
 // })
+// .then(response => response.json())
+// .then(result => console.log(result))
+// .catch(error => console.log('error', error));
+
+// $.ajax({
+//     url: "https://ssa-api.toyotavn.com.vn/api/TokenAuth/Authenticate",
+//     type: 'POST',
+//     headers: {
+//         'Abp.TenantId': '1005',
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json',
+//         "charset":"utf-8",
+//         "Access-Control-Allow-Origin": "*",
+//         "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+//     },
+//     data: {
+//         mode: 'same-origin',
+//         raw:
+//         {
+//             "userNameOrEmailAddress": "API_LEAD",
+//             "password": "123123"
+//         },
+//         mode: 'no-cors'
+//     },
+//     success: function(res) {
+//         console.log(res);
+//     }
+// })
+// let header := w.Header()
+// header.Add("Access-Control-Allow-Origin", "*")
+// header.Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
+// header.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+// async function postData(url = '', data = {}) {
+//     // Default options are marked with *
+//     const response = await fetch(url, {
+//       method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//     //   mode: 'cors', // no-cors, *cors, same-origin
+//     //   cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//     //   credentials: 'same-origin', // include, *same-origin, omit
+//         headers: {
+//             // "Access-Control-Allow-Origin": "*",
+//             'Abp.TenantId': '1005',
+//             'Content-Type': 'application/json',
+//         },
+//         mode: 'no-cors',
+//     //   redirect: 'follow', // manual, *follow, error
+//     //   referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//       body: JSON.stringify(data) // body data type must match "Content-Type" header
+//     });
+//     console.log(url);
+//     return response; // parses JSON response into native JavaScript objects
+//   }
+
+//   postData('https://ssa-api.toyotavn.com.vn/api/TokenAuth/Authenticate', {
+//             mode: 'raw',
+//             raw:
+//             {
+//                 "userNameOrEmailAddress": "API_LEAD_HAKUHOKO",
+//                 "password": "XLjm8W#~94"
+//             }
+//         })
+// .then(reponseBis => reponseBis)
+// .then(dataBis => console.log(dataBis));;
+
+// function login () {
+
+
+// let tokenUrl;
+// tokenUrl = 'https://ssa-api-test.toyotavn.com.vn/api/TokenAuth/Authenticate';
+// fetch(tokenUrl, {
+//     method: "POST",
+//     headers: new Headers({
+//         'Abp.TenantId': '1005',
+//         'Content-Type': 'application/json'
+//     }),
+//     body: {
+//         mode: 'raw',
+//         raw:
+//         {
+//             "userNameOrEmailAddress": "API_LEAD",
+//             "password": "123123"
+//         }
+//     }
+// })
 // .then(response => response.text())
 // .then(result => console.log(result))
 // .catch(error => console.log('error', error));
+
+// }
+
+// login ()
+
+// axios.post('https://ssa-api.toyotavn.com.vn/api/TokenAuth/Authenticate', {
+//     mode: 'raw',
+//     raw:
+//     {
+//         "userNameOrEmailAddress": "API_LEAD",
+//         "password": "XLjm8W#~94"
+// }
+// },
+
+// {
+//     headers: {
+//          'Abp.TenantId': '1005',
+//     'Content-Type': 'application/json'
+//         }
+   
+// })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+
+var myHeaders = new Headers();
+myHeaders.append("Authorization", "Bearer TokenAccess");
+myHeaders.append("Content-Type", "application/json");
+
+// var raw = JSON.stringify({
+//   "date": "2022-07-06",
+//   "time": "11:02",
+//   "model": "Innova",
+//   "leadSource": "TMV - Online (Website)",
+//   "whenCustomerWantToBuy": "Trong vòng 10 ngày",
+//   "dealer": "TPT",
+//   "firstName": "Nguyễn Trần",
+//   "phoneNumber": "09660212482",
+//   "email": "",
+//   "username": "",
+//   "campaign": ""
+// });
+
+// var requestOptions = {
+//   method: 'GET',
+//   headers: myHeaders,
+// //   body: raw,
+//   redirect: 'follow'
+// };
+
+// fetch("https://ssa-api.toyotavn.com.vn/api/services/app/SalesCustomerWeb/GetListLeadSources", requestOptions)
+//   .then(response => response.text())
+//   .then(result => console.log(result))
+//   .catch(error => console.log('error', error));
+
+
+
+
+var settings = {
+    "url": "https://ssa-api.toyotavn.com.vn/api/services/app/SalesCustomerWeb/GetListLeadSources",
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+      "Authorization": "Bearer TokenAccess"
+    },
+  };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
